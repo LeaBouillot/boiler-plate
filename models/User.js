e2fb530bcb92f507b315b31f69b9fab22c1df5d6
bestdevelope <leabouillot7@gmail.com>
2024-11-10 15:43:17 +0100

    update

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const salt = require("salt");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const userSchema = new mongoose.Schema({
+// monoose schema
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -14,33 +14,58 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    match: /.+\@.+\..+/,
+    trim: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
   },
   password: {
     type: String,
     required: true,
-    minlength: 6,
+    minlength: 8,
+    maxlength: 100,
+    select: false, // hide the password in the response
+    // validate: {
+    //   validator: (value) =>
+    //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+    //       value
+    //     ),
+    //   message:
+    //     "Password must contain at least 8 characters, including uppercase letters, lowercase letters, numbers, and special characters.",
+    // },
+  },
+  role: {
+    type: Number,
+    default: 0,
+    // 0: user, 1: admin
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExpiration: {
+    type: Number,
   },
 });
 
+// Hash the password before saving it to the database
 userSchema.pre("save", function (next) {
-  // only hash the password if it has been modified (i.e. not from a pre-save hook)
+  // Only hash the password if it has been modified
   if (!this.isModified("password")) return next();
 
   // Generate a salt
-  bcrypt.genSalt(saltRounds, function (err, salt) {
+  bcrypt.genSalt(saltRounds, (err, salt) => {
     if (err) return next(err);
-    // Store hash in your password DB.
-    bcrypt.hash(this.password, salt, function (err, hash) {
+
+    // Hash the password using the generated salt
+    bcrypt.hash(this.password, salt, (err, hash) => {
       if (err) return next(err);
+      // Assign the hashed password to the user object
       this.password = hash;
-      next(); // continue saving the user.
+      next(); // Continue saving the user
     });
   });
 });
+console.log("Password before hashing:", this.password);
 
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
-
-//
