2b83134d242fac693cabbcf0f168697c73d654bc
bestdevelope <leabouillot7@gmail.com>
2024-11-10 14:42:28 +0100

    Store hash password in DB

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const salt = require("salt");
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,6 +23,24 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre("save", function (next) {
+  // only hash the password if it has been modified (i.e. not from a pre-save hook)
+  if (!this.isModified("password")) return next();
+
+  // Generate a salt
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
+    // Store hash in your password DB.
+    bcrypt.hash(this.password, salt, function (err, hash) {
+      if (err) return next(err);
+      this.password = hash;
+      next(); // continue saving the user.
+    });
+  });
+});
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
+
+//
